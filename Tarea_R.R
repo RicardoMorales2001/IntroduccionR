727af80af27d363176f7ee8113310ee17ffb6bc3
Ricardo Morales <angel.morales@est.ikiam.edu.ec>
2022-02-26 21:56:19 -0600

    Se realizao el literal 2

--- Tarea_R.R
+++ Tarea_R.R
@@ -2,9 +2,5 @@
 #EJERCICIO 1
 Ciudad <- "ambato"
 #Ejercicio 2
-paises_cordillera_andes <- c("Argentina", "Bolivia", "Chile", "Colombia", "Ecuador", "Peru", "Venezuela")
-Población_paises_De_America <- data.frame(identador = seq(1:12),
-                                          norte_sur = as.factor(c(rep(0, 10), 1, 1)),
-                                          paises = c ("Argentina", "Bolivia", "Brasil", "Chile", "Colombia", "Ecuador", "Paraguay", 
-                                                      "Peru", "Surinam", "Venezuela", "USA", "Mexico"))
-data.frame(DNase)
+paises_cordillera_andes <- c ("Argentina", "Bolivia", "Chile", "Colombia", "Ecuador", "Peru", "Venezuela")
+
