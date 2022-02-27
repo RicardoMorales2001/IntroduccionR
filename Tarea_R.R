a2c784ba9fdf8cd8a662a2889ea89e352301b204
Ricardo Morales <angel.morales@est.ikiam.edu.ec>
2022-02-26 22:08:42 -0600

    Se realizao el literal 3

--- Tarea_R.R
+++ Tarea_R.R
@@ -4,3 +4,10 @@ Ciudad <- "ambato"
 #Ejercicio 2
 paises_cordillera_andes <- c ("Argentina", "Bolivia", "Chile", "Colombia", "Ecuador", "Peru", "Venezuela")
 
+#Ejercicio 3
+Autos_con_los_litros_de_su_motor<- data.frame(identador = seq(1:12),
+                                              Costo_mayor_a_200K_dÓlares = as.factor(c(rep(0, 10), 1, 1)),
+                                              Autos = c ("Porsche 991 GT2 RS", "BAC Mono", "Koenigsegg Regera", "Caterham Seven 620R", "SSC Ultimate Aero TT", "Fenyr SuperSport", "Porsche 992 Turbo S", "Bugatti Chiron", "McLaren P1",
+                                                          "Aston Martin Valkyrie", "Caparo T1", "Bugatti Veyron Super Sport"),
+                                              Litros_del_motor = c (3.8, 2.3, 4.3, 2.0, 6.9, 5.1, 3.7, 4.0, 2.6, 2.9, 3.6, 5.0 )) #Litros
+
