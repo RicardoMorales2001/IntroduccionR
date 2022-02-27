12efb4d659577cd9bd6cba9f1429d228efb7caf0
Ricardo Morales <angel.morales@est.ikiam.edu.ec>
2022-02-26 21:24:13 -0600

    Script R

---
+++ Tarea_R.R
@@ -0,0 +1,10 @@
+#Tarea Bioinformatica
+#EJERCICIO 1
+Ciudad <- "ambato"
+#Ejercicio 2
+paises_cordillera_andes <- c("Argentina", "Bolivia", "Chile", "Colombia", "Ecuador", "Peru", "Venezuela")
+Población_paises_De_America <- data.frame(identador = seq(1:12),
+                                          norte_sur = as.factor(c(rep(0, 10), 1, 1)),
+                                          paises = c ("Argentina", "Bolivia", "Brasil", "Chile", "Colombia", "Ecuador", "Paraguay", 
+                                                      "Peru", "Surinam", "Venezuela", "USA", "Mexico"))
+data.frame(DNase)
